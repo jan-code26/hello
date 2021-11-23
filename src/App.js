5e1716dff7aa65584f01a45371f890364fd34de0
jahnavi <jahnavipatel0206@gmail.com>
2021-11-23 19:00:36 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import 'react-native-paper';
+import OnboardingScreen from './screens/OnboardingScreen/OnboardingScreen';
+import StartScreen from './screens/StartScreen/StartScreen';
+import { NavigationContainer } from '@react-navigation/native';
+ import { createStackNavigator } from '@react-navigation/stack';
+ import AsyncStorage from '@react-native-community/async-storage';
+ import { useEffect } from 'react';
+ import { ALREADY_LAUNCHED } from './config/config.js';
+
+ const AppStack = createStackNavigator();
+
+ const App = () => {
+     const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+     useEffect(() => {
+         AsyncStorage.getItem(ALREADY_LAUNCHED).then(value => {
+             if(value == null) {
+                 AsyncStorage.setItem(ALREADY_LAUNCHED, 'true');
+                 setIsFirstLaunch(true);
+             } else {
+                 setIsFirstLaunch(false);
+
+            }
+        });
+
+    }, []);
+
+    if(isFirstLaunch === null){
+        return null;
+    } else if (isFirstLaunch === true) {
+        return (
+            <NavigationContainer>
+            <AppStack.Navigator
+            headerMode= "none"
+            >
+                <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+                <AppStack.Screen name="Start" component={StartScreen} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+        );
+    } else {
+        return <StartScreen />;
+    }
+}
+
+export default App;
